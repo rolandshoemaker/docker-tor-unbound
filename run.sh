d0807ae80b3f89e71547cd04ca3b9729567caa36
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-01-14 18:50:13 -0800

    ?

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# iptables -t filter -p udp --dport 53 --insert OUTPUT -j D
+iptables -t filter -p udp --dport 53 --insert OUTPUT -j DROP
 tor -f /etc/tor/torrc &
-proxychains unbound -d -v
+PROXYCHAINS_SOCKS5=9050 proxychains unbound -d -v
